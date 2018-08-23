898cb0fc287d55ff9b7ff0fb9de489971559b57c
Gabor Barany <gabor@cleenup.com>
2018-08-23 23:16:36 +0100

    docker mount permanent store

--- docker-build-run.sh
+++ docker-build-run.sh
@@ -1,2 +1,2 @@
 docker build -t "orderbook-analyser" .
-docker run -it "orderbook-analyser"
+docker run -it --mount type=bind,source="$(pwd)"/results,target=/app/results --name "orderbook-analyser-instance" "orderbook-analyser"
