900bd6b305cacb669e11e887e3a04559f321701d
Gabor Barany <gabor@cleenup.com>
2018-08-25 13:55:42 +0100

    removing obsolete stuff

--- docker-build-run.sh
+++
@@ -1,4 +0,0 @@
-docker build -t "orderbook-analyser" .
-docker rm orderbook-analyser-instance
-#docker run -it --mount type=bind,source="$(pwd)"/results,target=/app/results -e MODE='LIVE' --name "orderbook-analyser-instance" "orderbook-analyser"
-docker run -it --mount type=bind,source="$(pwd)"/results,target=/app/results -e MODE='SIMDB' --name "orderbook-analyser-instance" "orderbook-analyser"
