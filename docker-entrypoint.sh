49a993c8146c6a3c942130f80adbfd0fa73e7332
Gabor Barany <gabor@cleenup.com>
2018-08-25 15:59:29 +0100

    limit removed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,6 @@ if [ "$Mode" = "LIVE" ]; then
     python ./src/FrameworkLive.py --noplot --resultsdir=./results/
 else
     if [ "$Mode" = "SIMDB" ]; then
-        python ./src/FrameworkSimDB.py --resultsdir=./results/ --limit 5000
+        python ./src/FrameworkSimDB.py --resultsdir=./results/ #--limit=5000
     fi
 fi
