4882ba1225a97f1b04d480f1547d94a9e48c9d60
Gabor Barany <gabor@cleenup.com>
2018-08-28 00:16:11 +0100

    trader async loop change

--- src/Trader.py
+++ src/Trader.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
-
 import ccxt.async_support as ccxt
+from InitLogger import *
 
 class Trader:
     def __init__(self, exchangeNames=[],credfile='./cred/api.json'):
@@ -19,13 +19,12 @@ class Trader:
     
     def fetchBalances(self):
         self.balance = {}
-
+        tasks=[]
         for exchangeName in self.exchangeNames:
-            asyncio.ensure_future(self.fetchBalance(exchangeName))
+            tasks.append(asyncio.ensure_future(self.fetchBalance(exchangeName)))
 
-        pending = asyncio.Task.all_tasks()
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.gather(*pending))
+        loop.run_until_complete(asyncio.gather(*tasks))
         
     def getFreeBalance(self,exchangeName,symbol):
         return float(self.balance[exchangeName][symbol]["free"])
@@ -34,33 +33,36 @@ class Trader:
 
     async def marketBuyOrder(self,exchangeName,symbol,amount):
         exchange = getattr(ccxt, exchangeName)(self.keys[exchangeName])
+        response={}
         if exchange.has['createMarketOrder']:
             try:
                 response= await exchange.createLimitBuyOrder(symbol, amount)
-            except Exception as e:
-                print('Failed to create order with', exchange.id, type(e).__name__, str(e))
+            except (ccxt.ExchangeError, ccxt.NetworkError) as error:
+                logger.error('creatingLimitBuyOrder failed from '+exchangeName+" "+symbol+": "+ type(error).__name__+" "+ str(error.args))
         else:
-            print("No market oder possible on",exchangeName)
+            logger.error("No market oder possible on "+exchangeName)
         await exchange.close()
         return response
 
     def marketBuyOrders(self,tradelist):
+        orders = []
         for trade in tradelist:
             exchangeName = trade[0]
             symbol = trade[1]
             amount = trade[2]
-            asyncio.ensure_future(self.marketBuyOrder(exchangeName,symbol,amount))
+            orders.append(asyncio.ensure_future(self.marketBuyOrder(exchangeName,symbol,amount)))
 
-        pending = asyncio.Task.all_tasks()
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.gather(*pending))
+        loop.run_until_complete(asyncio.gather(*orders))
 
 if __name__ == "__main__":
+    logger.info("hello world")
     trader = Trader(exchangeNames=["kraken","gdax","bitstamp"],credfile='./cred/api.json')
-    #trader.fetchBalances()
-    #print(trader.getFreeBalance("kraken","BTC"))
+    trader.fetchBalances()
+    logger.info("Free balance on GDAX:"+str(trader.getFreeBalance("gdax","BTC")))
     tradelist = [
-        ("kraken","BTC",0.0001)
+        #("gdax","BTC/USD",0.0001),
+        ("bitstamp","BTC/USD",0.0001)
     ]
 
     trader.marketBuyOrders(tradelist)
