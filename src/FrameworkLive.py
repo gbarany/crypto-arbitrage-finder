b0419653857666235cfde1968a71e4fa9560eb13
Gabor Barany <gabor@cleenup.com>
2018-08-28 00:15:48 +0100

    logger code to a shared file

--- src/FrameworkLive.py
+++ src/FrameworkLive.py
@@ -9,24 +9,7 @@ import time
 from OrderbookAnalyser import OrderbookAnalyser
 import threading
 import datetime
-import logging
-
-logger = logging.getLogger('CryptoArbitrageApp')
-logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-fh = logging.FileHandler('CryptoArbitrageApp.log')
-fh.setLevel(logging.DEBUG)
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
-
+from InitLogger import *
 
 async def pollOrderbook(exchange,symbols):
     i = 0    
