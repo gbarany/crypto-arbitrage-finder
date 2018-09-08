94a7481aaa2470449839b834b533a7a2ca37ae91
Gabor Barany <gabor@cleenup.com>
2018-09-08 17:54:28 +0100

    orderbook analyser unit test fix

--- src/OrderbookAnalyser_test.py
+++ src/OrderbookAnalyser_test.py
@@ -2,6 +2,7 @@ import pytest
 from OrderbookAnalyser import OrderbookAnalyser
 
 from threading import Condition, Thread
+import time
 
 arbTradeTriggerEvent = Condition()
 arbTradeQueue = []
@@ -44,19 +45,26 @@ class TestClass(object):
     def test_one(self):
 
         orderbookAnalyser = getOrderbookAnalyser()
-        cmc=getCMCSampleFetch()
-        orderbookAnalyser.updateCmcPrice(cmc)
-        orderbookAnalyser.update('kraken','BTC/USD',bids=[[9000,1]],asks=[[10000,1]],id=1,timestamp=100)
-        orderbookAnalyser.update('kraken','ETH/USD',bids=[[100,10]],asks=[[200,10]],id=2,timestamp=101)
-        orderbookAnalyser.update('kraken','ETH/BTC',bids=[[1/4,10]],asks=[[1/5,10]],id=3,timestamp=102)
 
-        arbTradeTriggerEvent.acquire()
-        TRADE_EVENT_TRIGGERED = arbTradeTriggerEvent.wait(2)            
-        arbTradeTriggerEvent.release()
+        def feedSamples():
+            time.sleep(0.2)
+            cmc=getCMCSampleFetch()
+            orderbookAnalyser.updateCmcPrice(cmc)
+            orderbookAnalyser.update('kraken','BTC/USD',bids=[[9000,1]],asks=[[10000,1]],id=1,timestamp=100)
+            orderbookAnalyser.update('kraken','ETH/USD',bids=[[100,10]],asks=[[200,10]],id=2,timestamp=101)
+            orderbookAnalyser.update('kraken','ETH/BTC',bids=[[1/4,10]],asks=[[1/5,10]],id=3,timestamp=102)
+        
+        Thread(target=feedSamples).start()
+
+        for i in range(3):
+            arbTradeTriggerEvent.acquire()
+            TRADE_EVENT_TRIGGERED = arbTradeTriggerEvent.wait(1)            
+            arbTradeTriggerEvent.release()
             
 
         assert TRADE_EVENT_TRIGGERED == True
-
+        assert len(arbTradeQueue)==3
+        
 if __name__ == "__main__":
     tc=TestClass()
     tc.test_one()
