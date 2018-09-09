64f562109b8072a14f05abf6690013c2c55a07c2
Gabor Barany <gabor@cleenup.com>
2018-09-09 12:38:06 +0100

    unit test updates

--- src/ArbitrageGraph_test.py
+++ src/ArbitrageGraph_test.py
@@ -2,18 +2,38 @@ import pytest
 import numpy as np
 from ArbitrageGraph import ArbitrageGraph
 
-
+from OrderBook import OrderBookPrice
 class TestClass(object):
 
     def test_one(self):
         arbitrageGraph = ArbitrageGraph()
-        p1=arbitrageGraph.updatePoint(symbol="BTC/USD",exchangename="kraken",fee_rate=0.0,l_ask=10000,h_bid=9000,timestamp=0)
+        p1=arbitrageGraph.updatePoint(
+            symbol="BTC/USD",
+            exchangename="kraken",
+            feeRate=0.0,
+            askPrice=OrderBookPrice(meanprice=10000,limitprice=10000,vol_BASE=1),
+            bidPrice=OrderBookPrice(meanprice=9000,limitprice=9000,vol_BASE=1),
+            timestamp=0)
         assert p1.isNegativeCycle == False
         
-        p2=arbitrageGraph.updatePoint(symbol="ETH/USD",exchangename="kraken",fee_rate=0.0,l_ask=200,h_bid=100,timestamp=1)
+        p2=arbitrageGraph.updatePoint(
+            symbol="ETH/USD",
+            exchangename="kraken",
+            feeRate=0.0,
+            askPrice=OrderBookPrice(meanprice=200,limitprice=200,vol_BASE=1),
+            bidPrice=OrderBookPrice(meanprice=100,limitprice=100,vol_BASE=1),
+            timestamp=1)
+
         assert p2.isNegativeCycle == False
         
-        p3=arbitrageGraph.updatePoint(symbol="BTC/ETH",exchangename="kraken",fee_rate=0.0,l_ask=5,h_bid=4,timestamp=2)
+        p3=arbitrageGraph.updatePoint(
+            symbol="BTC/ETH",
+            exchangename="kraken",
+            feeRate=0.0,
+            askPrice=OrderBookPrice(meanprice=5,limitprice=5,vol_BASE=1),
+            bidPrice=OrderBookPrice(meanprice=4,limitprice=4,vol_BASE=1),
+            timestamp=2)
+
         assert p3.isNegativeCycle == True
 
         with pytest.raises(ValueError):
@@ -24,16 +44,37 @@ class TestClass(object):
 
         path=arbitrageGraph.getPath(nodes=["kraken-BTC","kraken-USD","kraken-ETH","kraken-BTC"],timestamp=4)
         assert path.edges_age_s == [4,3,2]
-        assert path.edges_weight==[-np.log(9000),-np.log(1/200),-np.log(1/5)]
+        assert path.edges_weight==[9000,1/200,1/5]
         assert path.hops==3
         assert path.exchanges_involved==['kraken']
         assert path.nof_exchanges_involved==1
 
     def test_TTLTest_one(self):
         arbitrageGraph = ArbitrageGraph(edgeTTL=5)
-        arbitrageGraph.updatePoint(symbol="BTC/USD",exchangename="kraken",fee_rate=0.0,l_ask=10000,h_bid=9000,timestamp=0)
-        arbitrageGraph.updatePoint(symbol="ETH/USD",exchangename="kraken",fee_rate=0.0,l_ask=200,h_bid=100,timestamp=1)
-        arbitrageGraph.updatePoint(symbol="BTC/ETH",exchangename="kraken",fee_rate=0.0,l_ask=5,h_bid=4,timestamp=2)
+        
+        arbitrageGraph.updatePoint(
+            symbol="BTC/USD",
+            exchangename="kraken",
+            feeRate=0.0,
+            askPrice=OrderBookPrice(meanprice=10000,limitprice=10000,vol_BASE=1),
+            bidPrice=OrderBookPrice(meanprice=9000,limitprice=9000,vol_BASE=1),
+            timestamp=0)
+
+        arbitrageGraph.updatePoint(
+            symbol="ETH/USD",
+            exchangename="kraken",
+            feeRate=0.0,
+            askPrice=OrderBookPrice(meanprice=200,limitprice=200,vol_BASE=1),
+            bidPrice=OrderBookPrice(meanprice=100,limitprice=100,vol_BASE=1),
+            timestamp=1)
+
+        arbitrageGraph.updatePoint(
+            symbol="BTC/ETH",
+            exchangename="kraken",
+            feeRate=0.0,
+            askPrice=OrderBookPrice(meanprice=5,limitprice=5,vol_BASE=1),
+            bidPrice=OrderBookPrice(meanprice=4,limitprice=4,vol_BASE=1),
+            timestamp=2)
 
         with pytest.raises(ValueError):
             arbitrageGraph.getPath(nodes=["kraken-BTC","kraken-USD","kraken-ETH","kraken-BTC"],timestamp=6)
@@ -41,18 +82,20 @@ class TestClass(object):
 
     def test_TTLTest_two(self):
         arbitrageGraph = ArbitrageGraph(edgeTTL=5)
-        arbitrageGraph.updatePoint(symbol="BTC/USD",exchangename="kraken",fee_rate=0.0,l_ask=10000,h_bid=9000,timestamp=0)
-        arbitrageGraph.updatePoint(symbol="ETH/USD",exchangename="kraken",fee_rate=0.0,l_ask=200,h_bid=100,timestamp=3)
-        arbitrageGraph.updatePoint(symbol="BTC/ETH",exchangename="kraken",fee_rate=0.0,l_ask=5,h_bid=4,timestamp=4)
+        arbitrageGraph.updatePoint(symbol="BTC/USD",exchangename="kraken",feeRate=0.0,askPrice=OrderBookPrice(meanprice=10000,limitprice=10000,vol_BASE=1),bidPrice=OrderBookPrice(meanprice=9000,limitprice=9000),timestamp=0)
+        arbitrageGraph.updatePoint(symbol="ETH/USD",exchangename="kraken",feeRate=0.0,askPrice=OrderBookPrice(meanprice=200,limitprice=200,vol_BASE=1),bidPrice=OrderBookPrice(meanprice=100,limitprice=100),timestamp=3)
+        arbitrageGraph.updatePoint(symbol="BTC/ETH",exchangename="kraken",feeRate=0.0,askPrice=OrderBookPrice(meanprice=5,limitprice=5,vol_BASE=1),bidPrice=OrderBookPrice(meanprice=4,limitprice=4),timestamp=4)
         
         with pytest.raises(ValueError):
             arbitrageGraph.getPath(nodes=["kraken-BTC","kraken-USD","kraken-ETH","kraken-BTC"],timestamp=6)
 
-        arbitrageGraph.updatePoint(symbol="BTC/USD",exchangename="kraken",fee_rate=0.0,l_ask=12000,h_bid=5000,timestamp=5)
+        arbitrageGraph.updatePoint(
+            symbol="BTC/USD",exchangename="kraken",feeRate=0.0,askPrice=OrderBookPrice(meanprice=12000,limitprice=12000,vol_BASE=1),
+            bidPrice=OrderBookPrice(meanprice=5000,limitprice=5000,vol_BASE=1),timestamp=5)
 
         path=arbitrageGraph.getPath(nodes=["kraken-BTC","kraken-USD","kraken-ETH","kraken-BTC"],timestamp=6)
         assert path.edges_age_s == [1,3,2]
-        assert path.edges_weight==[-np.log(5000),-np.log(1/200),-np.log(1/5)]
+        assert path.edges_weight==[5000,1/200,1/5]
         assert path.hops==3
         assert path.exchanges_involved==['kraken']
         assert path.nof_exchanges_involved==1
--- src/OrderbookAnalyser_test.py
+++ src/OrderbookAnalyser_test.py
@@ -1,16 +1,19 @@
 import pytest  
 from OrderbookAnalyser import OrderbookAnalyser
+from Trade import Trade
 
 from threading import Condition, Thread
 import time
 
 arbTradeTriggerEvent = Condition()
 arbTradeQueue = []
+#vol_BTC=[1,0.1,0.01]
+vol_BTC=[1]
 
 @pytest.fixture(scope="class")
 def getOrderbookAnalyser():
   return OrderbookAnalyser(
-        vol_BTC=[1,0.1,0.01],
+        vol_BTC=vol_BTC,
         edgeTTL=30,
         priceTTL=60,
         tradeLogFilename='tradelog_live_test.csv',
@@ -25,17 +28,17 @@ def getCMCSampleFetch():
     ticker['BTC/USD']={
         'symbol':'BTC/USD',
         'timestamp':100,
-        'last':6500}
+        'last':9000}
     
     ticker['ETH/USD']={
         'symbol':'ETH/USD',
         'timestamp':100,
-        'last':6500}
+        'last':100}
 
     ticker['ETH/BTC']={
         'symbol':'ETH/BTC',
         'timestamp':100,
-        'last':6500}
+        'last':0.03}
 
     return ticker
 
@@ -45,26 +48,38 @@ class TestClass(object):
     def test_one(self):
 
         orderbookAnalyser = getOrderbookAnalyser()
-
+        cmc=getCMCSampleFetch()
+        
         def feedSamples():
             time.sleep(0.2)
-            cmc=getCMCSampleFetch()
             orderbookAnalyser.updateCmcPrice(cmc)
             orderbookAnalyser.update('kraken','BTC/USD',bids=[[9000,1]],asks=[[10000,1]],id=1,timestamp=100)
-            orderbookAnalyser.update('kraken','ETH/USD',bids=[[100,10]],asks=[[200,10]],id=2,timestamp=101)
-            orderbookAnalyser.update('kraken','ETH/BTC',bids=[[1/4,10]],asks=[[1/5,10]],id=3,timestamp=102)
+            orderbookAnalyser.update('kraken','ETH/USD',bids=[[100,1000]],asks=[[200,1000]],id=2,timestamp=101)
+            orderbookAnalyser.update('kraken','ETH/BTC',bids=[[0.03,1000]],asks=[[0.04,1000]],id=3,timestamp=102)
         
         Thread(target=feedSamples).start()
 
-        for i in range(3):
+        for i in range(len(vol_BTC)):
             arbTradeTriggerEvent.acquire()
-            TRADE_EVENT_TRIGGERED = arbTradeTriggerEvent.wait(1)            
+            #TRADE_EVENT_TRIGGERED = arbTradeTriggerEvent.wait(1)
+            TRADE_EVENT_TRIGGERED = arbTradeTriggerEvent.wait()
             arbTradeTriggerEvent.release()
+            assert (i,TRADE_EVENT_TRIGGERED) == (i,True)
             
 
-        assert TRADE_EVENT_TRIGGERED == True
-        assert len(arbTradeQueue)==3
+        assert len(arbTradeQueue)==len(vol_BTC)
         
+        path=arbTradeQueue[0]
+        tradeList=path.toTradeList()
+        trade=tradeList[0]
+        assert (trade.symbol,trade.amount,trade.price,trade.tradetype,trade.status) == ('BTC/USD',vol_BTC[0],9000,Trade.SELL_ORDER,Trade.STATUS_INITIAL)
+
+        trade=tradeList[1]
+        assert (trade.symbol,trade.amount,trade.price,trade.tradetype,trade.status) == ('ETH/USD',vol_BTC[0]/cmc['ETH/BTC']['last'],200,Trade.BUY_ORDER,Trade.STATUS_INITIAL)
+
+        trade=tradeList[2]
+        assert (trade.symbol,trade.amount,trade.price,trade.tradetype,trade.status) == ('ETH/BTC',vol_BTC[0]/cmc['ETH/BTC']['last'],0.03,Trade.SELL_ORDER,Trade.STATUS_INITIAL)
+
 if __name__ == "__main__":
     tc=TestClass()
     tc.test_one()
