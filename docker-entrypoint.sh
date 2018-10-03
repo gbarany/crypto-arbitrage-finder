929b63d62f13d2bf9d4bd17f551dfeb40f87b1d6
Gabor Barany <gabor@cleenup.com>
2018-10-04 00:45:24 +0100

    adding neo4j support to the docker container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-Mode="${MODE:-LIVE}"
+Mode="${MODE:-LIVEWITHNEO4J}"
 
 if [ "$Mode" = "LIVE" ]; then
     python ./src/FrameworkLive.py --noplot --resultsdir=./results/
 else
     if [ "$Mode" = "SIMDB" ]; then
         python ./src/FrameworkSimDB.py --resultsdir=./results/ #--limit=5000
+    else
+        if [ "$Mode" = "LIVEWITHNEO4J" ]; then
+            python ./src/FrameworkLive.py --noplot --resultsdir=./results/ --neo4jmode=aws
+        fi
     fi
 fi
--- docker-run-live-with-neo4j.sh
+++ docker-run-live-with-neo4j.sh
@@ -2,5 +2,5 @@ docker rm orderbook-analyser-instance-live
 docker run -it \
     --mount type=bind,source="$(pwd)"/results,target=/app/results \
     --mount type=bind,source="$(pwd)"/cred,target=/app/cred \
-    -e MODE='LIVE' \
+    -e MODE='LIVEWITHNEO4J' \
     --name "orderbook-analyser-instance-live" "orderbook-analyser"
