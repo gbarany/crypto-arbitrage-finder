0661561e442f4dfb205028be0a0a76dede4a7f1d
Gabor Barany <gabor@cleenup.com>
2018-10-08 00:06:41 +0100

    pytest to generate coverage xlm

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,1 @@
-py.test --cov=src src/*_test.py --cov-config .coveragerc -v
-#py.test --cov=src src/ArbitrageGraph_test.py --cov-config .coveragerc -v
+py.test --cov=src src/*_test.py --cov-config .coveragerc -v --cov-report=xml:cov.xml
