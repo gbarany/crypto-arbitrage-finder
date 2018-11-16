21aa5c89c6fdaa16675c76859008e1eda60396e7
Gabor Barany <gabor@cleenup.com>
2018-11-16 23:09:52 +0000

    unit test for extra-extrachange deals

--- src/OrderbookAnalyser_test.py
+++ src/OrderbookAnalyser_test.py
@@ -90,3 +90,53 @@ class TestClass(object):
             orderRequestList = orderRequestLists[0][0]
             assert (orderRequestList.market, orderRequestList.amount, orderRequestList.price, orderRequestList.type,orderRequestList.getStatus()) == \
                     ('ETH/BTC',vol_BTC[0] / cmc['ETH/BTC']['last'], 0.03,OrderRequestType.SELL, OrderRequestStatus.INITIAL)
+
+
+    def test_crossExchange(self,monkeypatch, mocker):
+        feeRates = [0]#[0.02]
+        for feeRate in feeRates:
+            def getTakerFeeMock(self,exchangename, symbol):
+                    return feeRate
+            monkeypatch.setattr(FeeStore, 'getTakerFee', getTakerFeeMock)
+            
+            orderbookAnalyser = getOrderbookAnalyser()
+            cmc = getCMCSampleFetch()
+            orderbookAnalyser.arbitrageGraphNeo.graphDB.resetDBData()
+            
+            mocker.spy(orderbookAnalyser.trader, 'execute')
+
+            orderbookAnalyser.updateCoinmarketcapPrice(cmc)
+            orderbookAnalyser.update(
+                'kraken',
+                'BTC/USD',
+                bids=[[9000, 1]],
+                asks=[[10000, 1]],
+                timestamp=100)
+            orderbookAnalyser.update(
+                'kraken',
+                'ETH/USD',
+                bids=[[100, 1000]],
+                asks=[[200, 1000]],
+                timestamp=101)
+            orderbookAnalyser.update(
+                'binance',
+                'ETH/BTC',
+                bids=[[0.03, 1000]],
+                asks=[[0.04, 1000]],
+                timestamp=102)
+
+
+            assert orderbookAnalyser.trader.execute.call_count == len(vol_BTC)
+            orderRequestLists = orderbookAnalyser.trader.execute.call_args_list[0][0][0].getOrderRequestLists()
+            
+            orderRequestList = orderRequestLists[1].getOrderRequests()[0]
+            assert (orderRequestList.market, orderRequestList.amount, orderRequestList.price, orderRequestList.type,orderRequestList.getStatus()) == \
+                    ('BTC/USD', vol_BTC[0], 9000, OrderRequestType.SELL,OrderRequestStatus.INITIAL)
+
+            orderRequestList = orderRequestLists[1].getOrderRequests()[1]
+            assert (orderRequestList.market, orderRequestList.amount, orderRequestList.price, orderRequestList.type,orderRequestList.getStatus()) == \
+                    ('ETH/USD',vol_BTC[0] / cmc['ETH/BTC']['last'], 200,OrderRequestType.BUY, OrderRequestStatus.INITIAL)
+
+            orderRequestList = orderRequestLists[0].getOrderRequests()[0]
+            assert (orderRequestList.market, orderRequestList.amount, orderRequestList.price, orderRequestList.type,orderRequestList.getStatus()) == \
+                    ('ETH/BTC',vol_BTC[0] / cmc['ETH/BTC']['last'], 0.03,OrderRequestType.SELL, OrderRequestStatus.INITIAL)
