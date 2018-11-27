9c700f47c3a6568e91e6ce7348aa10718eb84147
Gabor Barany <gabor@cleenup.com>
2018-11-27 20:30:43 +0000

    docker file for live trading

---
+++ LIVE-docker-run-netx-kafkaAWS.sh
@@ -0,0 +1,6 @@
+docker run --rm -it \
+    --mount type=bind,source="$(pwd)"/results,target=/app/results \
+    --mount type=bind,source="$(pwd)"/cred,target=/app/cred \
+    -e MODE='LIVE_NETX_KAFKA_AWS' \
+    --name "orderbook-analyser-instance-live" "orderbook-analyser"
+    --live
