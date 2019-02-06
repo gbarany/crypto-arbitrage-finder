3eb28486c1879e17433af2a2c1a5a46ad879d665
Gabor <gabor.barany@icloud.com>
2019-02-06 22:15:06 +0000

    data formatting

--- tools/GocryptotraderWebsocket.py
+++ tools/GocryptotraderWebsocket.py
@@ -1,10 +1,10 @@
 import asyncio
-import pathlib
-import ssl
 import websockets
 import json
 import logging
 import sys
+import dateutil.parser
+import time
 
 # Init logger
 logger = logging.getLogger('testLogger')
@@ -20,23 +20,21 @@ logger.addHandler(ch)
 
 
 async def gocryptotraderWebSocket():
-    async with websockets.connect('wss://18.185.134.222:9050') as ws:
-        async for message in ws:
+    async with websockets.connect('ws://18.185.134.222:9050/ws') as ws:
+        async for msg in ws:
             try:
-                #msg = json.loads(message)
-                #symbol = msg['recipient'].split('.')[2].upper()
-                #symbol = symbol[0:3]+"/"+symbol[3:]
-                #payload = {}
-                #payload['exchange'] = "gocryptotrader"
-                #payload['symbol'] = symbol
-                #payload['data'] = {}
-                #payload['data']['asks'] = list(map(lambda entry:entry[0:2],msg['payload']['asks']))
-                #payload['data']['bids'] = list(map(lambda entry:entry[0:2],msg['payload']['bids']))
-                #payload['timestamp'] = msg['timestamp']/1e9
-                #logger.info("Received " + symbol +  " prices from gocryptotrader")
-                print(message)
+                message = json.loads(msg)
+                if message['Event'] == 'orderbook_update':
+                    symbol = message['Data']['pair']['first_currency']+"/" + message['Data']['pair']['second_currency']
+                    payload = dict()
+                    payload['exchange'] = message['exchange'].lower()
+                    payload['symbol'] = symbol
+                    payload['data'] = {}
+                    payload['data']['asks'] = list(map(lambda entry: [float(entry['Price']), float(entry['Amount'])], message['Data']['asks']))
+                    payload['data']['bids'] = list(map(lambda entry: [float(entry['Price']), float(entry['Amount'])], message['Data']['bids']))
+                    payload['timestamp'] = time.mktime(dateutil.parser.parse(message['Data']['last_updated']).timetuple())
+                    logger.info("Received " + symbol + " prices from "+message['exchange'].lower())
             except Exception as error:
-                logger.warn("Error while parsing gocryptotrader websocket data")
-                #logger.warn("Error while parsing gocryptotrader websocket data: "+ type(error).__name__ + " " + str(error.args))
+                logger.warning("Error while parsing websocket data: " + type(error).__name__ + " " + str(error.args))
 
 asyncio.get_event_loop().run_until_complete(gocryptotraderWebSocket())
