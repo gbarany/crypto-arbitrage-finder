3ad462d517b76bc62e58dc0b4f22b45ab10bce68
Gabor Barany <gabor@cleenup.com>
2019-02-28 23:25:17 +0000

    multiprocessing

--- tools/coinbaseproWebSocket.py
+++ tools/coinbaseproWebSocket.py
@@ -4,6 +4,7 @@ import logging
 import sys
 import dateutil.parser
 import time
+from multiprocessing import Process, Event
 
 # Init logger
 logger = logging.getLogger('coinbaseproTestLogger')
@@ -28,34 +29,36 @@ class CryptoArbOrderBook(cbpro.OrderBook):
         return super().__init__(product_id=product_id, log_to=log_to)
 
     def on_message(self, message):
-        if (time.time() - self.timeLastRun)<self.timeLimiterSeconds:
+        if (time.time() - self.timeLastRun) < self.timeLimiterSeconds:
+            return super().on_message(message)
+        try:
+            book = self.get_current_book()
+            if len(book["asks"])>0 and len(book["bids"])>0:
+                asksConsolidated = self.getConsolidatedOrderbook(book["asks"], reverse=False)
+                bidsConsolidated = self.getConsolidatedOrderbook(book["bids"], reverse=True)
+                print("asks:"+str(asksConsolidated)+", bids:"+str(bidsConsolidated))
+
+                if self.asksConsolidatedOld != asksConsolidated or self.bidsConsolidatedOld != bidsConsolidated:
+                    payload = {}
+                    payload['exchange'] = "coinbasepro"
+                    payload['symbol'] = message['product_id'].replace('-','/')
+                    payload['data'] = {}
+                    payload['data']['asks'] = asksConsolidated
+                    payload['data']['bids'] = bidsConsolidated
+                    payload['timestamp'] = time.mktime(dateutil.parser.parse(message['time']).timetuple())
+                    logger.info("Received " + payload['symbol'] + " prices from coinbasepro")
+                    self.asksConsolidatedOld = asksConsolidated
+                    self.bidsConsolidatedOld = bidsConsolidated
+
+                if book["asks"][0][0]<=book["bids"][-1][0]:
+                    logger.error("Bid higher than ask")
+        except Exception as err:
+            logger.error("Error during message processing:" + str(err))
+        finally:
+            self.timeLastRun = time.time()
             return super().on_message(message)
 
-        book=self.get_current_book()
-        if len(book["asks"])>0 and len(book["bids"])>0:
-            asksConsolidated = self.getConsolidatedOrderbook(book["asks"], reverse=False)
-            bidsConsolidated = self.getConsolidatedOrderbook(book["bids"], reverse=True)
-            print("asks:"+str(asksConsolidated)+", bids:"+str(bidsConsolidated))
-
-            if self.asksConsolidatedOld != asksConsolidated or self.bidsConsolidatedOld != bidsConsolidated:
-                payload = {}
-                payload['exchange'] = "coinbasepro"
-                payload['symbol'] = message['product_id'].replace('-','/')
-                payload['data'] = {}
-                payload['data']['asks'] = asksConsolidated
-                payload['data']['bids'] = bidsConsolidated
-                payload['timestamp'] = time.mktime(dateutil.parser.parse(message['time']).timetuple())
-                logger.info("Received " + payload['symbol'] + " prices from coinbasepro")
-                self.asksConsolidatedOld = asksConsolidated
-                self.bidsConsolidatedOld = bidsConsolidated
-
-
-            if book["asks"][0][0]<=book["bids"][-1][0]:
-                logger.error("Bid higher than ask")
-        self.timeLastRun = time.time()
-        return super().on_message(message)
-
-    def getConsolidatedOrderbook(self,entries,reverse=False):
+    def getConsolidatedOrderbook(self, entries, reverse=False):
         orderbook = []
         
         size = Decimal(0)
@@ -80,18 +83,28 @@ class CryptoArbOrderBook(cbpro.OrderBook):
         
         return orderbook
         
-    
-#pairs = ['BCH/BTC', 'BTC/EUR', 'LTC/EUR', 'BTC/USD', 'BTC/EUR', 'ETH/USD','ETH/EUR', 'BCH/EUR', 'ETH/BTC', 'BCH/USD']
-pairs = ['BTC/USD']
-# start orderbooks
-ordersbooks = []
-for pair in pairs:
-    orderbook=CryptoArbOrderBook(product_id=pair.replace('/','-'))
-    ordersbooks.append(orderbook)
+def CryptoArbOrderBookProcess(pair,stopProcessesEvent):
+    orderbook=CryptoArbOrderBook(product_id=pair.replace('/', '-'))
     orderbook.start()
-
-time.sleep(10000)
-
-# stop orderbooks
-for orderbook in ordersbooks:
+    stopProcessesEvent.wait()
     orderbook.close()
+
+pairs = ['BCH/BTC', 'BTC/EUR', 'LTC/EUR', 'BTC/USD', 'BTC/EUR', 'ETH/USD','ETH/EUR', 'BCH/EUR', 'ETH/BTC', 'BCH/USD']
+
+stopProcessesEvent = Event()
+processes = [Process(target=CryptoArbOrderBookProcess, args=(pair,stopProcessesEvent)) for pair in pairs]
+
+# start order books
+for process in processes:
+    process.daemon = True
+    process.start()
+
+input("Press Enter to stop")
+
+stopProcessesEvent.set()
+for process in processes:
+    process.join()
+
+logger.info("coinbaseproWebsocket exited normally. Bye.")
+
+
